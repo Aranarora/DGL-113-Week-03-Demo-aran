0be3ed5714fbb3a5861fa48f38f9156afe3aa72c
Aranarora <aranarora007@gmail.com>
2023-01-30 13:51:13 -0800

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -4,28 +4,31 @@ let count = 0;
 let items = [];
 
 
-function clearItem(){
-    count = 0;
-    items = [];
-    const cleartheItemElement = document.getElementById('todos');
-    cleartheItemElement.innerHTML = '';
-   
-}
+
 
 function addItem() {
 
     console.log("add item");
-    const durationElement = document.getElementById('duration');
-    const duration = durationElement.value;
+    let durationElement = document.getElementById('duration');
+    let duration = durationElement.value;
     console.log(duration);
-    const descriptionElement = document.getElementById('description');
-    const description = descriptionElement.value;
+    let descriptionElement = document.getElementById('description');
+    let description = descriptionElement.value;
     console.log(description);
 
     count += 1;
 
 
-    items.push(`<tr><td>${count}</td><td>${duration}</td><td>${description}</td></tr>`);
+    items.push(`<tr><th>${count}</th><td>${duration}</td><td>${description}</td></tr>`);
 
 let todosElement = document.getElementById('todos');
-todosElement.innerHTML = items.join('');}
+todosElement.innerHTML = items.join( '' );
+}
+
+function clearItem(){
+    count = 0;
+    items = [];
+    let cleartheItemElement = document.getElementById('todos');
+    cleartheItemElement.innerHTML = '';
+   
+}
